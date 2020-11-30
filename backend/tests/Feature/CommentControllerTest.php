e046e068f3c50ad2d3c3f4cdc73874cef6bd6380
shosho <saitama.shohei@gmail.com>
2020-11-30 17:18:46 +0900

    (WIP)コメントテスト

--- backend/tests/Feature/CommentControllerTest.php
+++ backend/tests/Feature/CommentControllerTest.php
@@ -1,21 +1,23 @@
 <?php
+
 namespace Tests\Feature;
 
 use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class PostControllerTest extends TestCase
+class CommentControllerTest extends TestCase
 {
   /**
    * A basic feature test example.
-   * 
+   *
    * @return void
    */
-  public function testIndex()
+  public function testExample()
   {
     $this->withoutExceptionHandling();
     // id1のユーザーをログイン状態にさせ、getでposts.indexにリクエストを送る
-    $response = $this->actingAs(User::find(1))->get(route('posts.index'));    
+    $response = $this->actingAs(User::find(1))->get(route('posts.comment'));
     // 引数に入ってるリクエストコードを受け取れてるか確認する
     $response->assertStatus(200);
   }
