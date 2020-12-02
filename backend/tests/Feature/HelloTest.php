661c91e2f32573ffbf97da7f309eba7f32d11674
shosho <saitama.shohei@gmail.com>
2020-12-02 10:38:09 +0900

    (WIP)ユニットテスト練習

--- backend/tests/Feature/HelloTest.php
+++ backend/tests/Feature/HelloTest.php
@@ -3,28 +3,28 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class HelloTest extends TestCase
 {
-  /**
-   * A basic feature test example.
-   *
-   * @return void
-   */
+  use DatabaseMigrations;
+
   public function testHello()
   {
-    $this->withoutExceptionHandling();
-    $this->assertTrue(true);
-    
-    $arr = [];
-    $this->assertEmpty($arr);
 
-    $msg = "Hello";
-    $this->assertEquals('Hello', $msg);
+    $response = $this->get('/');
+    $response->assertStatus(200);
 
-    $n = random_int(0, 100);
-    $this->assertLessThan(100, $n);
+    $response = $this->get('/hello');
+    $response->assertStatus(302);
+
+    $user = factory(User::class)->create();
+    $response = $this->actingAs($user)->get('/hello');
+    $response->assertStatus(200);
+
+    $response = $this->get('/no_route');
+    $response->assertStatus(404);
   }
 }
