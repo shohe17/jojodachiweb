8e875e4e51e74b93ce131115426a427dc9ed1580
shosho <saitama.shohei@gmail.com>
2020-12-05 10:21:59 +0900

    メソッド編集

--- backend/tests/Feature/PostControllerTest.php
+++ backend/tests/Feature/PostControllerTest.php
@@ -6,17 +6,16 @@ use Tests\TestCase;
 
 class PostControllerTest extends TestCase
 {
-  /**
-   * A basic feature test example.
-   * 
-   * @return void
-   */
   public function testIndex()
   {
-    $this->withoutExceptionHandling();
-    // id1のユーザーをログイン状態にさせ、getでposts.indexにリクエストを送る
-    $response = $this->actingAs(User::find(1))->get(route('posts.index'));    
-    // 引数に入ってるリクエストコードを受け取れてるか確認する
-    $response->assertStatus(200);
+      // 準備 userを作る
+      $user = User::factory()->make();
+      // ログイン状態している
+      $this->actingAs($user);
+      // 実行
+      $response = $this->get(route('posts.index'));
+
+      $response->assertStatus(200);
   }
+
 }
